f75fedfb445b221f1d08c9ecea7db419bbcffdc1
hadiakhan785 <hadiakhan785@gmail.com>
2018-11-04 21:14:06 +0500

    second commit

--- data.js
+++ data.js
@@ -2,44 +2,50 @@ let products = [
   {
     productName: "Cell Tech - Muscletech",
     imgURL: "image1.jpeg",
-    productDetail: "",
-    price: ""
+    productDetail: "<li>Increase muscle size and strength</li> <li>Cell-volumizing amino acids</li> <li>Formulated for hardgainers</li> <li>Maximize muscle creatine</li>",
+    price: "Rs. 4800/-"
   },
   {
     productName: "Nitro Tech - Muscletech",
     imgURL: "image2.jpeg",
-    productDetail: "",
-    price: ""
+    productDetail: "<li>Whey isolate & peptides as primary source</li> <li>Builds 70% more lean muscle than regular whey</li> <li>Superior to other whey protein formulas</li> <li>Amplifies recovery, performance & strength</li>",
+    price: "Rs. 13000/-"
   },
   {
     productName: "Platinum Creatine - Muscletech",
     imgURL: "image3.jpeg",
-    productDetail: "",
-    price: ""
+    productDetail: " <li>Increase muscle size</li> <li>Enhance strength</li> <li>Improve recovery</li>",
+    price: "Rs. 1800/-"
   },
   {
     productName: "C4 Original - Cellucor",
     imgURL: "image4.jpeg",
-    productDetail: "",
-    price: ""
+    productDetail: "<li>Explosive energy</li> <li>Heightened focus and an overwhelming urge to tackle any challenge</li> <li>Ranging from beginners all the way up to the elite competitors</li>",
+    price: "4700/-"
   },
   {
     productName: "Serious Mass - ON",
     imgURL: "image6.jpeg",
-    productDetail: "",
-    price: ""
+    productDetail: "<li>High Calorie Weight Gainer: 1,250 Calories per 2-Scoop Serving</li> <li>50 Grams of Protein per Serving</li> <li>Over 250 Grams of Carbohydrates per Serving</li> <li>25 Vitamins & Minerals Plus Creatine & Glutamine</li> <li>Using an Electric Blender to Add Fresh or Frozen Fruits, Peanut Butter, Nuts & Other Calorie Dense Foods to Boost Your Weight Gain Potential</li>",
+    price: "(6lbs) Rs. 5500/-, (12lbs) Rs. 8000/- "
   },
   {
     productName: "Gold Standard 100% Whey - ON",
     imgURL: "image7.jpeg",
-    productDetail: "",
-    price: ""
+    productDetail: "<li>Whey Protein Microfractions from Whey Protein Isolates & Ultra-Filtered Whey Protein Concentrate</li>",
+    price: "Rs. 7500/-"
   },
   {
     productName: "Hydroxy Cut Hardcore Elite - Muscletech",
     imgURL: "image8.jpeg",
-    productDetail: "",
-    price: ""
+    productDetail: "<li>Supports increased focus with its key thermogenic driver.</li> <li>Do not exceed recommended dosage and refer to the warning statement below</li> <li>One serving of this product contains up to as much caffeine as 3 cups of coffee.</li>",
+    price: "Rs. 3000/-"
+  },
+  {
+    productName: "Scivation Xtend BCAA Powder, Branched Chain Amino Acids, BCAAs, 30 Servings",
+    imgURL: "slider-pic-2.jpg",
+    productDetail: "<li>7g of BCAAs - 2:1:1 Research-Proven Ratio to Help Support Muscle Growth & Strength</li> <li>Sugar Free, Carbohydrate Free & Zero Calories</li> <li>3.5g of Leucine for Muscle Protein Synthesis</li> <li>2.5g of L-Glutamine & 1g of Citrulline Malate</li> <li>Stay Hydrated Pre-, During, and Post Workout with Electrolytes in Xtend</li>",
+    price: "Rs. 3200/-"
   }
 ];
 
--- script.js
+++ script.js
@@ -7,13 +7,8 @@
 function init() {
   var mySwiper = new Swiper(".swiper-container", {
     // Optional parameters
-    direction: "vertical",
+    direction: "horizontal",
     loop: true,
-
-    // If we need pagination
-    // pagination: {
-    //   el: '.swiper-pagination',
-    // },
 
     // Navigation arrows
     navigation: {
@@ -21,17 +16,28 @@ function init() {
       prevEl: ".swiper-button-prev"
     },
 
+    // for autoplay
     autoplay: {
       delay: 2500,
       disableOnInteraction: false
     },
 
-    effect: "flip",
-    grabCursor: true
-
-    // And if we need scrollbar
-    // scrollbar: {
-    //   el: '.swiper-scrollbar',
-    // },
   });
+
+  let productsContainer = document.querySelector('.product-container');
+
+  for(let i = 0; i < products.length; i++){
+      productsContainer.innerHTML += ` <div class="card">
+                        <img class="card-img-top" src="images/${products[i].imgURL}" alt="Card image cap">
+                        <div class="card-body">
+                            <h6 class="card-title">${products[i].productName}</h6>
+                            <p class="card-text">
+                                <ul class="product-details">
+                                    ${products[i].productDetail}
+                                </ul>
+                            </p>
+                            <p class="card-text product-price">Price: <span class="price">${products[i].price}</span></p>
+                        </div>
+                    </div>`;
+  }
 }
